8a9134982bead63f829a7245b134b83d920c965e
Jacqueline Offei <offeijacqueline@gmail.com>
2024-09-17 15:48:36 +0000

    adding javascript updates

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,23 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register');
+console.log(registerForm)
 // handle form submit event
-registerForm.onsubmit = function() {
-    event.preventDefault()
-    console.log(event);
+registerForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    // collect form data
+    const formData =new FormData (registerForm);
+//    send data to backend
+//   display success message to the user
+    const messageH1 = document.querySelector('#message'); messageH1.textContent = 'Account Registered  Successfully';
+    console.log(messageH1);
+});
+
+// declare variable age
+// let age = 71
+// define square age function
+function squareAge(age) {
+    return age ** 2;
 }
+
+const squaredAge = squareAge(54)
+console.log(squaredAge);
